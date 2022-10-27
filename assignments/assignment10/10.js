47f778690fbc7f0e54e54628b3579eba7f8ce2b2
Grantwilliam1 <89321263+Grantwilliam1@users.noreply.github.com>
2022-10-27 12:49:46 -0600

    I hate json

---
+++ assignments/assignment10/10.js
@@ -0,0 +1,16 @@
+const section = document.querySelector('section');
+
+//problem 1
+
+const jsonFile = require('10.json')
+console.log(jsonFile);
+
+//problem 2
+
+//problem 3
+
+//problem 4
+
+//problem 5
+
+//problem 6
