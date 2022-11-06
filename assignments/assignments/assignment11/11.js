20c7fbb829c54fbb68f2a1848e38d9a74a2fc25c
Grantwilliam1 <89321263+Grantwilliam1@users.noreply.github.com>
2022-11-06 10:42:16 -0700

    kittys :)

--- assignments/assignments/assignment11/11.js
+++ assignments/assignments/assignment11/11.js
@@ -2,22 +2,26 @@
 
 const quoteButton = document.querySelector('.button');
 quoteButton.addEventListener('click', getQuote);
+var color = ["#222f3e", "#f368e0", "#ee5283", "#CB2821", "#AEA04B", "#F75E25", "#F39F18", "#252850", "#FEF6C9"];
 
 const endpoint = 'https://api.thecatapi.com/v1/images/search';
 
 async function getQuote() {
+    let image = document.getElementById("cat")
+    fetch('https://api.thecatapi.com/v1/images/search')
+    .then(resp => resp.json())
+    .then(json => image.scr = json[0].url)
     console.log('Clicked button');
     let text = await fetch(endpoint);
     let response = await text.text();
     if (text){
         let json_response = JSON.parse(response);
         console.log('cats aquired');
-        console.log(json_response['url']);
-        displayQuote(json_response['url']);
+        console.log(response);
+        console.log(json_response[0]['url']);
+        displayQuote(json_response[0]['url']);
     }
 }
-
-//[{"id":"c7a","url":"https://cdn2.thecatapi.com/images/c7a.jpg","width":570,"height":760}]
 
 function displayQuote(x) {
     document.getElementById('cat').src = x;
@@ -25,30 +29,10 @@ function displayQuote(x) {
 
 getQuote();
 
-// const quoteButton = document.querySelector('.button');
-// quoteButton.addEventListener('click', getQuote);
-
-// const endpoint = 'https://api.whatdoestrumpthink.com/api/v1/quotes/random';
-
-// async function getQuote() {
-//     let text = await fetch(endpoint);
-//     let response = await text.text();
-
-//     let json_response = JSON.parse(response);
-//     console.log(json_response['message']);
-
-//     displayQuote(json_response['message']);
-// }
-
-
-
-// function displayQuote(x) {
-//     // const quoteBox = document.querySelector('#js-quote-text');
-//     // const textMessage = document.createTextNode(x);
-
-//     // quoteBox.appendChild(textMessage);
-
-//     document.getElementById('js-quote-text').textContent = x;
-// }
-
-// getQuote();
+const btn = document.getElementById('js-color');
+btn.addEventListener('click', function onClick(event) {
+    console.log('clicked');
+    const background = document.getElementById('background');
+    var randomNum = Math.floor(Math.random() * color.length);
+    background.style.backgroundColor = color[randomNum];
+});
