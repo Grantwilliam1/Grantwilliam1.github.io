e16d6cda566c50d01b46fb7400f3512a7f5a3089
Grantwilliam1 <89321263+Grantwilliam1@users.noreply.github.com>
2022-11-08 23:29:33 -0700

    wa12

--- js/wa12.js
+++ js/wa12.js
@@ -7,57 +7,43 @@ function submit() {
 }
 
 function reset() {
-    const resetvalue = 0000000000
-    output.textContent = resetvalue;
-}
-
-function plusRunner() {
-    plus(outputInt);
+    outputInt = 0;
+    output.textContent = outputInt;
 }
 
 function minus() {
-    output.textContent = outputInt - 1;
+    if (outputInt > 0){
+        outputInt -=1;
+        output.textContent = outputInt;
+    }
 }
 
 function plus() {
     if (outputInt < 9999999999){
         outputInt +=1;
-
+        output.textContent = outputInt;
     }
 }
 
-// function minus() {
-//     output.textContent = - 1;
-// }
+function random() {
+    outputInt = randomNumber(0, 9999999999);
+    output.textContent = outputInt;
+}
 
-// function plus() {
-//     output.textContent = + 1;
-// }
+function randomNumber(min, max){
+    const num = Math.floor(Math.random() * (max - min + 1)) + min;
+    return num;
+}
+
+const output = document.querySelector('.output');
+let outputInt = parseInt(output.textContent);
+console.log(outputInt);
 
 const minusButton = document.querySelector('.minus-button').addEventListener('click', minus);
 const plusButton = document.querySelector('.plus-button').addEventListener('click', plus);
 const resetButton = document.querySelector('.reset-button').addEventListener('click', reset);
+const randomButton = document.querySelector('.random-button').addEventListener('click', random);
 const submitButton = document.querySelector('.submit-button').addEventListener('click', submit);
-
-const output = document.querySelector('.output');
-const outputInt = parseInt(output.textContent);
-
-
-
-
-
-
-
-// const button = document.querySelector('.button');
-// const output = document.querySelector('.output');
-// let phone_content = document.querySelector('.phone');
-
-// button.addEventListener('click', updateOutput);
-
-// function updateOutput() {
-//     output.textContent = phone_content.value;
-//     alert(phone_content.value);
-// }
 
 var slider = document.getElementById('myRange');
 var sliderSubmit = document.querySelector(".slider-submit-button").addEventListener('click', update);
